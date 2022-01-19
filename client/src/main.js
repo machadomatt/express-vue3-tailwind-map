ba8dd486f857a6c905f5097bb218719785258c11
machadomatt <matheusmacsyn@gmail.com>
2022-01-19 13:22:59 -0300

    Setup Leaflet

--- client/src/main.js
+++ client/src/main.js
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/css/index.css";
+import "leaflet/dist/leaflet.css";
 
 createApp(App).use(router).mount("#app");
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,35 @@
 <template>
-  <div class="home"></div>
+  <div class="relative h-screen">
+    <div id="map" class="h-full z-[1]"></div>
+  </div>
 </template>
 
 <script>
+import leaflet from "leaflet";
+import { onMounted } from "vue";
 export default {
   name: "HomeView",
+  setup() {
+    onMounted(() => {
+      let map = leaflet
+        .map("map")
+        .setView([-23.079797902304385, -47.21396312836137], 10);
+
+      leaflet
+        .tileLayer(
+          `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${process.env.VUE_APP_MAPBOX_API_KEY}`,
+          {
+            attribution:
+              'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: "mapbox/streets-v11",
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: process.env.VUE_APP_MAPBOX_API_KEY,
+          }
+        )
+        .addTo(map);
+    });
+  },
 };
 </script>
