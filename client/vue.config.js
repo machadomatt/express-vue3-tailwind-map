abdd371fb57602fa54e4f17e1eb79651cc4544f1
machadomatt <matheusmacsyn@gmail.com>
2022-01-22 20:08:41 -0300

    Change output directory

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  outputDir: path.resolve(__dirname, "../server/public"),
   devServer: {
     proxy: {
       "/api": {
